7a638b0c080da3711f5f3462acdfdd00883aba99
Julien Delplanque <julien.delplanque@student.umons.ac.be>
2014-10-21 16:58:05 +0200

    Update the code so it is possible to implment unit tests.

--- jquery.github-activity.js
+++ jquery.github-activity.js
@@ -1,3 +1,111 @@
+var githubURL = 'https://github.com/'
+
+function userLink(user) {
+	return $('<a>', {
+		text: user.login,
+		href: githubURL + user.login
+	})
+}
+
+function repositoryLink(repo) {
+	return $('<a>', {
+		text: repo.name,
+		href: githubURL + repo.name
+	})
+}
+
+function branchName(ref) {
+	return ref.split('refs/heads/')[1]
+}
+
+function issueLink(issue) {
+	return $('<a>', {
+		text: 'issue ' + issue.number,
+		href: issue.html_url
+	})
+}
+
+function pullRequestLink(pull_request, comment) {
+	var url = pull_request.html_url
+
+	if(typeof comment !== 'undefined') {
+		url += '#issuecomment-' + comment.id
+	}
+
+	return $('<a>', {
+		text: 'pull request ' + pull_request.number,
+		href: url
+	})
+}
+
+function tag(payload) {
+	return $('<b>', {
+		text: payload.ref,
+	})
+}
+
+function releaseName(release) {
+	return $('<a>', {
+		text: release.name,
+		href: release.html_url
+	})
+}
+
+function createEventAction(event) {
+	if(event.payload.ref_type != 'tag')
+		return  'created respository ' + repositoryLink(event.repo).prop('outerHTML')
+	else
+		return 'created tag ' + tag(event.payload).prop('outerHTML') + ' in ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function followEventAction(event) {
+	return 'started following ' + userLink(event.payload.target).prop('outerHTML')
+}
+
+function watchEventAction(event) {
+	return event.payload.action + ' watching ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function pushEventAction(event) {
+	return 'pushed to ' + branchName(event.payload.ref) + ' at ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function issueCommentEventAction(event) {
+	return 'commented on ' + pullRequestLink(event.payload.issue, event.payload.comment).prop('outerHTML') + ' on ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function pullRequestEventAction(event) {
+	return 'opened ' + pullRequestLink(event.payload.pull_request).prop('outerHTML') + ' on ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function deleteEventAction(event) {
+	return 'deleted ' + event.payload.ref_type + ' ' + event.payload.ref + ' at ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function commitCommentEventAction(event) {
+	return 'commented on ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function issueEventAction(event) {
+	return 'opened ' + issueLink(event.payload.issue).prop('outerHTML') + ' on ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function forkEventAction(event) {
+	return 'forked ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function releaseEventAction(event) {
+	return 'created release ' + releaseName(event.payload.release).prop('outerHTML') + ' on ' + repositoryLink(event.repo).prop('outerHTML')
+}
+
+function gollumEventAction(event) {
+	return 'updated wiki of repository ' + repositoryLink(event.repository).prop('outerHTML')
+}
+
+function memberEventAction(event) {
+	return event.action + ' ' + userLink(event.member) + ' to ' + repositoryLink(event.repository)
+}
+
 (function($) {
 	$.fn.githubActivityFor = function(username, params) {
 		var githubURL = 'https://github.com/'
@@ -7,59 +115,6 @@
 			params['wrap'] = function(item) { return item }
 		}
 
-		var userLink = function(user) {
-			return $('<a>', {
-			    text: user.login,
-			    href: githubURL + user.login
-			})
-		}
-
-		var repositoryLink = function(repo) {
-			return $('<a>', {
-			    text: repo.name,
-			    href: githubURL + repo.name
-			})
-		}
-
-		var branchName = function(ref) {
-			return ref.split('refs/heads/')[1]
-		}
-
-		var issueLink = function(issue) {
-			return $('<a>', {
-			    text: 'issue ' + issue.number,
-			    href: issue.html_url
-			})
-		}
-
-		var pullRequestLink = function(pull_request, comment) {
-			var url = pull_request.html_url
-
-			if(typeof comment !== 'undefined') {
-				url += '#issuecomment-' + comment.id
-			}
-
-			return $('<a>', {
-			    text: 'pull request ' + pull_request.number,
-			    href: url
-			})
-		}
-
-		var tag = function(payload) {
-			return $('<b>', {
-				text: payload.ref,
-			})
-		}
-
-		var releaseName = function(release) {
-			return $('<a>', {
-				text: release.name,
-				href: release.html_url
-			})
-		}
-
-
-
 		var renderer = {
 			'CreateEvent': function(event) {
 				if(event.payload.ref_type != 'tag')
@@ -67,42 +122,18 @@
 				else
 					return 'created tag ' + tag(event.payload).prop('outerHTML') + ' in ' + repositoryLink(event.repo).prop('outerHTML')
 			},
-			'FollowEvent': function(event) {
-				return 'started following ' + userLink(event.payload.target).prop('outerHTML')
-			},
-			'WatchEvent': function(event) {
-				return event.payload.action + ' watching ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'PushEvent': function(event) {
-				return 'pushed to ' + branchName(event.payload.ref) + ' at ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'IssueCommentEvent': function(event) {
-				return 'commented on ' + pullRequestLink(event.payload.issue, event.payload.comment).prop('outerHTML') + ' on ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'PullRequestEvent': function(event) {
-				return 'opened ' + pullRequestLink(event.payload.pull_request).prop('outerHTML') + ' on ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'DeleteEvent': function(event) {
-				return 'deleted ' + event.payload.ref_type + ' ' + event.payload.ref + ' at ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'CommitCommentEvent': function(event) {
-				return 'commented on ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'IssuesEvent': function(event) {
-				return 'opened ' + issueLink(event.payload.issue).prop('outerHTML') + ' on ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'ForkEvent': function(event) {
-				return 'forked ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'ReleaseEvent': function(event) {
-				return 'created release ' + releaseName(event.payload.release).prop('outerHTML') + ' on ' + repositoryLink(event.repo).prop('outerHTML')
-			},
-			'GollumEvent': function(event) {
-				return 'updated wiki of repository ' + repositoryLink(event.repository).prop('outerHTML')
-			},
-			'MemberEvent': function(event) {
-				return event.action + ' ' + userLink(event.member) + ' to ' + repositoryLink(event.repository)
-			}
+			'FollowEvent': followEventAction,
+			'WatchEvent': watchEventAction,
+			'PushEvent': pushEventAction,
+			'IssueCommentEvent': issueEventAction,
+			'PullRequestEvent': pullRequestEventAction,
+			'DeleteEvent': deleteEventAction,
+			'CommitCommentEvent': commitCommentEventAction,
+			'IssuesEvent': issueCommentEventAction,
+			'ForkEvent': forkEventAction,
+			'ReleaseEvent': releaseEventAction,
+			'GollumEvent': gollumEventAction,
+			'MemberEvent': memberEventAction
 		}
 
 		this.each(function() {
